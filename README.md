721d2e940641b410cb15ac65fae8aae23caef9e2
Harakat Nabil <nabil.harakat@xayone.com>
2023-10-31 01:52:59 +0100

    create projet to generate jwt using java & spring boot

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jwt-service
+
+# Test a web service to generate a JWT using curl
+
+curl --location 'localhost:2020/ws-jwt/tokens' \
+--header 'Content-Type: application/json' \
+--data '{
+    "sub":"toto"
+}'
---
+++ src/main/java/com/jwt/JwtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtServiceApplication.class, args);
+	}
+
+}
